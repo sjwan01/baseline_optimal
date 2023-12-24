e2a7c8c6a6ede71bb6794462d7bbc9d1dec6aca0
Shunji Wan <shunjiwan2001@gmail.com>
2023-12-24 04:55:28 -0500

    fix build issues

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Shunji Wan'
 author = 'Shunji Wan'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.6'
+release = '0.0.7'
 
 
 # -- General configuration ---------------------------------------------------
